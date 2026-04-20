bbb89acf1cb14d50c9279ab74176523f1a64a60b
Akemi <liboad0@student.oaisd.org>
2026-04-20 14:00:06 -0400

    added more to the tutorial scene

--- Assets/Scripts/TutorialScrpit.cs
+++ Assets/Scripts/TutorialScrpit.cs
@@ -1,8 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TutorialScrpit : MonoBehaviour
 {
-    
+    [SerializeField] private bool hasPressedW = false;
+    [SerializeField] private bool hasPressedS = false;
+    [SerializeField] private bool hasPressedA = false;
+    [SerializeField] private bool hasPressedD = false;
+
+    [SerializeField] private bool hasJumped = false;
+
+    public Text tutorialText;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            hasPressedW = true;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            hasPressedS = true;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            hasPressedA = true;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            hasPressedD = true;
+        }
+
+        if(hasPressedW == true && hasPressedA == true && hasPressedS == true && hasPressedD == true && hasJumped != true)
+        {
+            tutorialText.text = "Nice job!\nNow press Space to jump over the wall.";
+            if (Input.GetKey(KeyCode.Space))
+            {
+                hasJumped = true;
+                tutorialText.text = "Awesome, now walk to the next area.";
+            }
+        }
+    }
 }
